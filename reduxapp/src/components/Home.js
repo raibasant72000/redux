dfef477bd5982962b27e31b47a5f8f613f58545c
Basanta <raibasant72000@gmail.com>
2022-01-05 02:29:06 -0500

    design done

--- reduxapp/src/components/Home.js
+++ reduxapp/src/components/Home.js
@@ -3,12 +3,11 @@ import carimg from "../Assets/car.jpg";
 import cartimg from "../Assets/cart.png";
 import "./Home.css";
 
-function Home (props){
-  console.log("Home:",props.data)
+function Home(props) {
+  console.log("Home:", props.data);
   return (
     <div className="container">
       <div className="add-to-cart">
- 
         <img className="cartsize" src={cartimg} alt="cartmg" />
       </div>
       <h1> Home component</h1>
@@ -21,13 +20,17 @@ function Home (props){
           <span>Price: $100000</span>
         </div>
         <div className="btn-wrapper item">
-          <button onClick={() => 
-            props.addToCartHandler({price:1000, name:"iphone11"})
-          }> Add to Cart
+          <button
+            onClick={() =>
+              props.addToCartHandler({ price: 1000, name: "iphone11" })
+            }
+          >
+            {" "}
+            Add to Cart
           </button>
         </div>
       </div>
     </div>
   );
-};
+}
 export default Home;
